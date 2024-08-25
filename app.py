2336ba6cbd38369eff2c514faaa570bfa4300fae
carolina romero <99673763+caromerou@users.noreply.github.com>
2024-08-25 10:21:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,35 @@ def authenticate(username, password):
 
 # Función para mostrar el formulario de inicio de sesión
 def show_login_form():
-    st.sidebar.header("Inicio de sesión")
+    st.sidebar.markdown("""
+    <style>
+        .login-container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+        }
+        .login-form {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            max-width: 300px;
+            text-align: center;
+        }
+        .login-form input {
+            margin-bottom: 10px;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+    
+    st.sidebar.markdown('<div class="login-container"><div class="login-form">', unsafe_allow_html=True)
+    
     username = st.sidebar.text_input("Nombre de usuario")
     password = st.sidebar.text_input("Contraseña", type="password")
+    
+    st.sidebar.markdown('</div></div>', unsafe_allow_html=True)
+    
     if st.sidebar.button("Iniciar sesión"):
         if authenticate(username, password):
             st.session_state.authenticated = True
@@ -122,3 +148,4 @@ if st.session_state.authenticated:
 else:
     # Muestra el formulario de inicio de sesión
     show_login_form()
+
